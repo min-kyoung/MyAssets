818cc07a73893f2e3c11741140fdfa9f49984562
min-kyoung <shalsrud9@naver.com>
2022-01-28 02:18:42 +0900

    SwiftUI - Tab Bar

--- MyAssets/ContentView.swift
+++ MyAssets/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 하단에 탭 바가 있는 TabView를 표현
+    @State private var selection: Tab = .asset
+    
+    // 먼저 선택된 탭이 항상 존재할 것이므로, 첫번째 탭으로 표현할 수 있도록 enum을 생성
+    enum Tab {
+        case asset
+        case recommend
+        case alert
+        case setting
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            Color.white
+                .tabItem {
+                    // 시스템 이미지로 나타냄
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tab.asset)
+            
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tab.recommend)
+            
+            Color.yellow
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알림")
+                }
+                .tag(Tab.alert)
+            
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }
     }
 }
 
